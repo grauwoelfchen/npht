af7758ca23f5b999f723feb823e695516a67976f
Yasuhiro Яша Asaka <yasuhiro.asaka@grauwoelfchen.net>
2020-06-03 13:04:25 +0900

    Finish exercise 7

--- src/bin/ex7.rs
+++ src/bin/ex7.rs
@@ -12,36 +12,64 @@ fn main() {
 
     let height = 3;
     let width = 10;
-    let start_y = (LINES() - height) / 2;
-    let start_x =(COLS() - width) / 2;
+    let mut start_y = (LINES() - height) / 2;
+    let mut start_x = (COLS() - width) / 2;
 
     waddstr(stdscr, "Press F1 to exit");
     refresh();
 
-    let mut my_win = newwin(height, width, start_y, start_x);
+    let mut my_win = create_newwin(height, width, start_y, start_x);
 
     loop {
         match getch() {
             KEY_F1 => break,
             KEY_LEFT => {
-                delwin(my_win);
-                my_win = newwin(height, width, start_y, start_x - 1);
+                destroy_win(my_win);
+                start_x -= 1;
+                my_win = create_newwin(height, width, start_y, start_x);
             },
             KEY_RIGHT => {
-                delwin(my_win);
-                my_win = newwin(height, width, start_y, start_x + 1);
+                destroy_win(my_win);
+                start_x += 1;
+                my_win = create_newwin(height, width, start_y, start_x);
             },
             KEY_UP => {
-                delwin(my_win);
-                my_win = newwin(height, width, start_y - 1, start_x);
+                destroy_win(my_win);
+                start_y -= 1;
+                my_win = create_newwin(height, width, start_y, start_x);
             },
             KEY_DOWN => {
-                delwin(my_win);
-                my_win = newwin(height, width, start_y + 1, start_x);
-            }
-            _ => {}
+                destroy_win(my_win);
+                start_y += 1;
+                my_win = create_newwin(height, width, start_y, start_x);
+            },
+            _ => {},
         }
     }
 
     endwin();
+}
+
+fn create_newwin(
+    height: i32,
+    width: i32,
+    start_y: i32,
+    start_x: i32,
+) -> WINDOW
+{
+    let local_win = newwin(height, width, start_y, start_x);
+    // print a border around the window
+    box_(local_win, 0, 0);
+    wrefresh(local_win);
+    local_win
+}
+
+fn destroy_win(local_win: WINDOW) {
+    // box_(local_win, ' ' as u32, ' ' as u32);
+    wborder(
+        local_win, ' ' as u32, ' ' as u32, ' ' as u32, ' ' as u32, ' ' as u32,
+        ' ' as u32, ' ' as u32, ' ' as u32,
+    );
+    wrefresh(local_win);
+    delwin(local_win);
 }
